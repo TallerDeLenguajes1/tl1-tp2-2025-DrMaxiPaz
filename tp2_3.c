dfee7f1e4e3d8dce627792bb0e63502dbb60b4c7
MaxiPaz <maxipaz0047@gmail.com>
2025-04-07 23:28:44 -0300

    punto 3 corregido

--- tp2_3.c
+++ tp2_3.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h> /* Para usar la función rand() */
 
 #define N 5
 #define M 7
 int main()
 {
+    srand(time(NULL)); // Inicializa la semilla para números aleatorios
     int i, j;
     int mt[N][M];
     int *pmt = &mt[0][0];
@@ -13,12 +15,9 @@ int main()
         for (j = 0; j < M; j++)
         {
             *(pmt + i * M + j) = 1 + rand() % 100;
-            printf("%lf ", *(pmt + i * M + j));
-            
+            printf("%d ", *(pmt + i * M + j));       
         }
         printf("\n");
     }
-
     return 0;
 }
-// corregir 
